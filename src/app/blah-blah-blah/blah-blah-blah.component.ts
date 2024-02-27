f3b1ade4b21405e94b9900f2ef8d77ae85db188d
Jason Beres <jasonberes@gmail.com>
2024-02-27 13:31:42 -0500

    HR Dashboard updates

--- src/app/blah-blah-blah/blah-blah-blah.component.ts
+++ src/app/blah-blah-blah/blah-blah-blah.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
+import { BoxOfficeRevenueType } from '../models/financial/box-office-revenue-type';
 import { EmployeesType } from '../models/northwind/employees-type';
+import { FinancialService } from '../services/financial.service';
 import { NorthwindService } from '../services/northwind.service';
 
 @Component({
-  selector: 'app-employees',
-  templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  selector: 'app-blah-blah-blah',
+  templateUrl: './blah-blah-blah.component.html',
+  styleUrls: ['./blah-blah-blah.component.scss']
 })
-export class EmployeesComponent implements OnInit, OnDestroy {
+export class BlahBlahBLahComponent implements OnInit, OnDestroy {
   private destroy$: Subject<void> = new Subject<void>();
+  public financialBoxOfficeRevenue: BoxOfficeRevenueType[] = [];
   public northwindEmployees: EmployeesType[] = [];
 
   constructor(
+    private financialService: FinancialService,
     private northwindService: NorthwindService,
   ) {}
 
   ngOnInit() {
+    this.financialService.getData('BoxOfficeRevenueType').pipe(takeUntil(this.destroy$)).subscribe({
+      next: (data) => this.financialBoxOfficeRevenue = data,
+      error: (_err: any) => this.financialBoxOfficeRevenue = []
+    });
     this.northwindService.getData('EmployeesType').pipe(takeUntil(this.destroy$)).subscribe({
       next: (data) => this.northwindEmployees = data,
       error: (_err: any) => this.northwindEmployees = []
